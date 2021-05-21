2e2e2ab0f8c1e0e0d6aa0656b60497741b6449d7
Antonio Valverde <antonio@valverdebalbuena.es>
2021-05-21 08:51:34 +0200

    Ejercicio 2.6 (phonebook)

---
+++ part02/phonebook/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const Personas = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.listado.map(tema => <li key={tema.name}>{tema.name}</li>)}  
+      </ul>
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleCambioNombre = evento => {
+      setNewName(evento.target.value);
+  }
+
+  const addNombre = (evento) => {
+      evento.preventDefault()
+      const nombreObjeto = {
+          name: newName
+      }
+      setPersons(persons.concat(nombreObjeto))
+      setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNombre}>
+        <div>
+          name: <input value={newName} onChange={handleCambioNombre}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Personas listado={persons} />
+    </div>
+  )
+}
+
+export default App
