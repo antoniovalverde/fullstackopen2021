bf2cc34c359e7f283d9ef9628699ac70cf1720f8
Antonio Valverde <antonio@valverdebalbuena.es>
2021-05-21 11:43:56 +0200

    Ejercicio 2.9 (phonebook)

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,55 +1,83 @@
 import React, { useState } from 'react'
 
 const Personas = (props) => {
+
+  const filtrados = props.listado.filter(
+    persona => persona.name.toLowerCase().includes(props.filtro.toLowerCase())
+  )
+
   return (
     <div>
       <ul>
-        {props.listado.map(tema => <li key={tema.name}>{tema.name}</li>)}  
+        {filtrados.map(tema => <li key={tema.name}>{tema.name}</li>)}  
       </ul>
     </div>
   )
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+      ])
   const [ newName, setNewName ] = useState('')
+  const [newNumero, setNewNumero] = useState('')
+  const [newFiltro, setNewFiltro] = useState('')
 
   const handleCambioNombre = evento => {
       setNewName(evento.target.value);
   }
 
-  const addNombre = (evento) => {
+  const handleCambioNumero = evento => {
+    setNewNumero(evento.target.value);
+  }
+
+  const handleFiltrarPersonas = evento => {
+    setNewFiltro(evento.target.value);
+
+
+  }
+
+  const addPersona = (evento) => {
       evento.preventDefault()
 
-      const found = persons.find(element => element.name === newName);
+      const foundNombre = persons.find(element => element.name === newName);
 
-      console.log(found)
-      if(found === undefined){
+      console.log(foundNombre)
+      if(foundNombre === undefined){
         const nombreObjeto = {
-          name: newName
+          name: newName,
+          number: newNumero
         }
         setPersons(persons.concat(nombreObjeto))
         setNewName('')
+        setNewNumero('')
       }else{
         alert(`${newName} is already added to phonebook`)
       }
-  }
+   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNombre}>
+      <div>
+          Filter Persons: <input onChange={handleFiltrarPersonas}/>
+      </div>
+      <br />
+      <form onSubmit={addPersona}>
         <div>
           name: <input value={newName} onChange={handleCambioNombre}/>
+          <br />
+          number: <input value={newNumero} onChange={handleCambioNumero}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Personas listado={persons} />
+      <Personas listado={persons} filtro={newFiltro}/>
     </div>
   )
 }
